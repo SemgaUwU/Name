e9de90e936d104811aa9fe1129f8dc1a57df249a
SemgaUwU <semga784@gmail.com>
2026-07-31 17:43:00 +0300

    Эксперемент один

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 let picture = document.querySelector('.pic')
+let text = document.querySelector('.text')
+
+picture.addEventListener('click', function() {
+    picture.style.visibility = 'hidden'
+})
